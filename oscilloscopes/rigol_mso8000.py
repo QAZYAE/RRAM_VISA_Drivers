c5c4b94a59d4a3fbd357cd197cc1b107dc3bc696
Lisa <tyutinaliza2003@gmail.com>
2026-06-16 15:11:38 +0300

    Oscilloscope driver edited

--- oscilloscopes/rigol_mso8000.py
+++ oscilloscopes/rigol_mso8000.py
@@ -36,10 +36,10 @@ class Rigol_MSO8000(VISA_instrument):
         """ Fetch data from oscilloscope screen
 
         Args:
-        channel (str, optional): Channel from which the waveform will be read: '1' | '2' | '3' | '4'.
-        form (str, optional): {WORD|BYTE|ASCii}
-        mode (str, optional): {NORMal|MAXimum|RAW}
-        apply (bool, optional): If True, the command is sent to the instrument immediately. If False,
+            channel (str, optional): Channel from which the waveform will be read: '1' | '2' | '3' | '4'.
+            form (str, optional): {WORD|BYTE|ASCii}
+            mode (str, optional): {NORMal|MAXimum|RAW}
+            apply (bool, optional): If True, the command is sent to the instrument immediately. If False,
             the command is appended to the `command_queue` and can be sent with `.send_queue()` method.
 
         Returns:
@@ -55,15 +55,15 @@ class Rigol_MSO8000(VISA_instrument):
         return self.query('wav:data?')
 
 
-    def parse_waveform(self, rawdata, xorigin: bool = False):
+    def parse_waveform(self, rawdata, xorigin: bool = True):
 
 
         """ Parse fetched data in ASCII
 
         Args:
-        rawdata (lst): raw data from the oscilloscope in ASCII format
-        channel (str, optional): Channel from which the waveform will be read: '1' | '2' | '3' | '4'.
-        apply (bool, optional): If True, the command is sent to the instrument immediately. If False,
+            rawdata (lst): raw data from the oscilloscope in ASCII format
+            channel (str, optional): Channel from which the waveform will be read: '1' | '2' | '3' | '4'.
+            apply (bool, optional): If True, the command is sent to the instrument immediately. If False,
             the command is appended to the `command_queue` and can be sent with `.send_queue()` method.
 
         Returns:
@@ -79,36 +79,112 @@ class Rigol_MSO8000(VISA_instrument):
             t0 = float(self.query('wav:xor?'))
         else:
             t0 = 0
-        time = np.arrange(t0, len(data), delta_t)
+        time = np.arange(t0, len(data), delta_t)
 
         return time, data
 
-    def auto_scale(self, apply: bool = False):
 
-        self.command('auto', apply=apply)
+    def auto_scale(self, apply: bool = True):
 
-        return
+        """ Enable the waveform auto setting function. The oscilloscope will automatically adjust the
+            vertical scale, horizontal time base, and trigger mode according to the input signal to
+            realize optimal waveform display.
+
+        Args:
+            apply (bool, optional): If True, the command is sent to the instrument immediately. If False,
+            the command is appended to the `command_queue` and can be sent with `.send_queue()` method.
+        """
+        return self.command('aut', apply=apply)
     
-    def run(self, apply: bool = False):
 
-        self.command('run', apply=apply)
+    def run(self, apply: bool = True):
 
-        return
+        """ Start the oscilloscope.
+        
+        Args:
+            apply (bool, optional): If True, the command is sent to the instrument immediately. If False,
+            the command is appended to the `command_queue` and can be sent with `.send_queue()` method.
+        """
+        return self.command('run', apply=apply)
     
-    def stop(self, apply: bool = False):
 
-        self.command('stop', apply=apply)
+    def stop(self, apply: bool = True):
 
-        return
+        """ Stop the oscilloscope.
+
+        Args:
+            apply (bool, optional): If True, the command is sent to the instrument immediately. If False,
+            the command is appended to the `command_queue` and can be sent with `.send_queue()` method.
+        """
+        return self.command('stop', apply=apply)
     
-    def single(self, apply: bool = False):
 
-        self.command('sing', apply=apply)
+    def single(self, apply: bool = True):
 
-        return
+        """ Set the trigger mode of the oscilloscope to "Single".
+
+        Args:
+            apply (bool, optional): If True, the command is sent to the instrument immediately. If False,
+            the command is appended to the `command_queue` and can be sent with `.send_queue()` method.
+        """
+        return self.command('sing', apply=apply)
     
-    def measure_amplitude(self, channel: int = 1, apply: bool = False):
 
-        self.command(f'meas:ams channel{channel}', apply=apply)
+    def measure_amplitude(self, channel: int = 1):
 
-        return self.query(f'meas:item vpp,chan{channel}')
+        """ Measure aplitude of the signal on channel.
+
+        Args:
+            channel (int): channel number on which the amplitude of the signal will be measured.
+
+        Returns:
+            amplitude (float)
+        """
+        if str(channel) not in ['1', '2', '3', '4']:
+            return f'ERROR: {self.resp}: wrong channel number: {channel}'
+        
+        return float(self.query(f'meas:stat:item? aver,vpp,chan{channel}'))
+    
+
+    def measure_phase_difference(self, channel: int = 1, ref_channel: int = 2):
+
+        """ Measure phase difference between signals on channel and ref_channel.
+
+        Args:
+            channel (int): channel number on which the phase of the signal will be measured.
+            ref_channel (int): chanel number with reference signal.
+
+        Returns:
+            amplitude (float)
+        """
+        if str(channel) not in ['1', '2', '3', '4']:
+            return f'ERROR: {self.resp}: wrong channel number: {channel}'
+        if str(ref_channel) not in ['1', '2', '3', '4']:
+            return f'ERROR: {self.resp}: wrong reference channel number: {channel}'
+        if str(ref_channel) == str(channel):
+            return f'ERROR: {self.resp}: channel number and reference channel number can`t be the same'
+        
+        return float(self.query(f'meas:stat:item? aver,rrdelay,chan{channel},chan{ref_channel}'))
+
+
+    def display_channels(self, channels: list = [1, 2], apply=True):
+        
+        """ Turns on specified channels and turns off the remaining channels.
+
+        Args:
+            channels (list): channels that need to be displayed: ['1', '2', '3', '4']
+            if channels is an empty list, all of the channels will be turned off.
+            apply (bool, optional): If True, the command is sent to the instrument immediately. If False,
+            the command is appended to the `command_queue` and can be sent with `.send_queue()` method.
+        """
+        off_channels = ['1', '2', '3', '4']
+
+        for channel in channels:
+            if str(channel) not in ['1', '2', '3', '4']:
+                return f'ERROR: {self.resp}: wrong channel number: {channel}'
+            self.command(f'chan{channel}:disp 1', apply=apply)
+            off_channels.remove(str(channel))
+
+        for channel in off_channels:
+            self.command(f'chan{channel}:disp 0', apply=apply)
+
